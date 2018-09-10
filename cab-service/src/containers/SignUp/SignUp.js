8ce256b930f91ad339eedff906076257ddcf2526
jinadi yasiruka <jinyyash@gmail.com>
2018-09-11 01:45:51 +0530

    signup finished

--- cab-service/src/containers/SignUp/SignUp.js
+++ cab-service/src/containers/SignUp/SignUp.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Input, Button,Card,CardBody } from 'mdbreact';
+import { Container, Row, Col, Input, Button, Card, CardBody } from 'mdbreact';
+import classes from './SignUp.css';
+import Center from 'react-center';
 
 class SignUp extends Component {
     State = {
@@ -9,28 +11,41 @@ class SignUp extends Component {
     }
     render() {
         return (
-            <Container style={{margintop:'50px'}} >
-                <Row>
-                    <Col md="6">
-                        <Card>
-                            <CardBody>
-                                <form>
-                                    <p className="h4 text-center py-4">Sign up</p>
-                                    <div className="grey-text">
-                                        <Input label="Your name" icon="user" group type="text" validate error="wrong" success="right" />
-                                        <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right" />
-                                        <Input label="Confirm your email" icon="exclamation-triangle" group type="text" validate error="wrong" success="right" />
-                                        <Input label="Your password" icon="lock" group type="password" validate />
-                                    </div>
-                                    <div className="text-center py-4 mt-3">
-                                        <Button color="red" type="submit">Register</Button>
-                                    </div>
-                                </form>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+
+            <div style={{padding: '50px 30px 30px 75px' ,    marginLeft:' 300px'
+        }}>
+              
+                    <Container   >
+
+                        <Row>
+                            <Col md="6">
+                                <Card >
+                                    <CardBody>
+                                        <form>
+                                            <h5 className="h4 text-center py-4"color="red" >Sign Up to Ride</h5>
+                                           <Center><h4>Safe, reliable rides in minutes</h4></Center> 
+                                            <div className="grey-text">
+                                                <Input label="Your name" icon="user" group type="text" validate error="wrong" success="right" />
+                                                <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right" />
+                                                <Input label="Confirm your email" icon="exclamation-triangle" group type="text" validate error="wrong" success="right" />
+                                                <Input label="Your password" icon="lock" group type="password" validate />
+                                            </div>
+                                            <div className="text-center py-4 mt-3">
+                                                <Button color="red" type="submit">Register</Button>
+                                            </div>
+                                        </form>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+
+                    </Container>
+                
+            </div>
+
+
+
+
         );
     }
 }
